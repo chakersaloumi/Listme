bf757d32aa32cdd8df97ec06f0cbb2e4973170ce
Chaker <Chaker@mes-imac-2.local>
2018-10-23 11:16:11 +0300

    Todo list complete with data source and delegate methods.

---
+++ Listme/ToDoListViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Listme
+//
+//  Created by Chaker on 10/23/18.
+//  Copyright © 2018 Chaker Saloumi. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggoes", "Destroy Demogorgan"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //Mark - Table View Data source Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true) // nicer
+        
+    }
+    
+
+
+}
+
