5694a9c1618cee7904ce62bbda96e421ac895334
Chaker <Chaker@mes-imac-2.local>
2018-10-29 12:45:19 +0200

    Concert to MVC design pattern

--- Listme/Controller/ToDoListViewController.swift
+++ Listme/Controller/ToDoListViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["Find Mike", "Buy Eggoes", "Destroy Demogorgan"]
+    var itemArray = [Items]()
+    
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
+
+        let newItem = Items()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem1 = Items()
+        newItem1.title = "Buy Eggos"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Items()
+        newItem2.title = "Destroy Demogorgan"
+        itemArray.append(newItem2)
+       
+
+        
+                if let items = defaults.array(forKey: "ToDoListArray") as? [Items] {
+                    itemArray = items
+        
         }
-    }
+    
 
     //Mark - Table View Data source Methods
     
@@ -30,7 +47,12 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+     
         
         return cell
         
@@ -39,11 +61,9 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
-        if (tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark){
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true) // nicer
         
@@ -57,10 +77,15 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "List me", style: .default) { (action) in
             // what happens when button on alert is clicked
+            let newItem = Items()
             
-           self.itemArray.append(textField.text!)
-            self.defaults.setValue(self.itemArray, forKey: "ToDoListArray")
-            self.tableView.reloadData() 
+            newItem.title = textField.text!
+            
+           self.itemArray.append(newItem)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
+            self.tableView.reloadData()
            
             
         }
---
+++ Listme/Model/Items.swift
@@ -0,0 +1,15 @@
+//
+//  items.swift
+//  Listme
+//
+//  Created by Chaker on 10/25/18.
+//  Copyright © 2018 Chaker Saloumi. All rights reserved.
+//
+
+import UIKit
+
+class Items {
+    var title: String = ""
+    var done: Bool = false
+    
+}
